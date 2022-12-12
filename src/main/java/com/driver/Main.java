7334cc43f2115e19111e243a6e595797eeb7823a
Swetha Shivani <cnk.swetha@gmail.com>
2022-12-12 19:26:51 +0530

    checking polumorphism:

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+public  void main(String [] args){
+            Product p = new Product();
+            p.product(10, 20);
+            p.product(10,10,10);
+            p.product(10.0, 20.0);
+}
+
 }
